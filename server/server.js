5ace101dc0e8404c529f93995be3051ba8bd5f7d
Liad Madmon <madmonl@post.bgu.ac.il>
2019-01-13 19:10:59 +0200

    added broadcast for new users joined in

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('New user connected')
 
+  socket.emit('newMessage', {
+    from: "Admin",
+    text: "Welcome to the chat"
+  })
+
+  socket.broadcast.emit('newMessage', {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', message => {
     console.log(message)
     io.emit('newMessage', {
@@ -24,8 +35,5 @@ io.on('connection', (socket) => {
   
   socket.on('disconnect', () => console.log('user disconnected'))
 })
-
-
-
 
 server.listen(port)
